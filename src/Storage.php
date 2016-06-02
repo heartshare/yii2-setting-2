e22c70c0e5cba560921ef590de9bbd36f6e58248
cartmanchen <csz@soyhw.com>
2016-06-02 15:45:53 +0800

    feat: except sections.

--- src/Storage.php
+++ src/Storage.php
@@ -8,11 +8,24 @@ abstract class Storage extends Component
 {
     public $sections = [];
 
+    public $except = [];
+
     public function save($section, $key, $value)
     {
-        if (!$this->sections || in_array($section, $this->sections)) {
+        if ($this->isMatch($section)) {
             return $this->store($section, $key, $value);
         }
+    }
+
+    protected function isMatch($section)
+    {
+        if ($this->except && in_array($section, $this->except)) {
+            return false;
+        }
+        if (!$this->sections || in_array($section, $this->sections)) {
+            return true;
+        }
+        return false;
     }
 
     abstract public function getData();
